2da1214dd47f002146b568201a78ce5f94d8649b
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-06-22 15:55:59 -0300

    Add project files.

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validacao de notificacoes de erro
+
+        // validacao de modelstate
+
+        // validacao da operacao de negocios
+    }
+}
